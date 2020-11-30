2d9aa0e8978d2cc21aa27213ccfa10db282696ef
Hossein Nassiri <hosseinnassiri2002@hotmail.com>
2020-11-29 19:19:34 -0500

    fixed removed settings class, upgraded message contracts project to net standard 2.1

--- src/MessageProcessor/BusConfiguration/MassTransitSettings.cs
+++ src/MessageProcessor/BusConfiguration/MassTransitSettings.cs
@@ -1,10 +1,15 @@
-﻿using MessageProcessor.BusConfiguration.AuditStore.MongoDb;
-
-namespace MessageProcessor.BusConfiguration
+﻿namespace MessageProcessor.BusConfiguration
 {
 	public sealed class MassTransitSettings
 	{
 		public RabbitMqSettings RabbitMqSettings { get; set; } = default!;
 		public MongoDbAuditStoreSettings MongoDbAuditStore { get; set; } = default!;
+	}
+
+	public sealed class MongoDbAuditStoreSettings
+	{
+		public string Connection { get; set; } = string.Empty;
+		public string DatabaseName { get; set; } = string.Empty;
+		public string CollectionName { get; set; } = string.Empty;
 	}
 }
